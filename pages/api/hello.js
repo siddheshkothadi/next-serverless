6fc213f88810f0985852f77f6e8e0206c8f90f1d
siddheshkothadi <realsiddheshko@gmail.com>
2022-10-16 11:37:11 +0530

    add loop condition

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  console.log("started");
+  for(let i = 0; i < 10000; i++) {
+    console.log(i);
+  }
+  console.log("ended");
+
   res.status(200).json({ name: 'John Doe' })
 }
